d404ae1a8561c1d7fbd632e5fc414dac16edb4de
saritamfd <sarita-milagros.flores-diaz.external@capgemini.com>
2023-04-25 10:59:18 +0200

    Añadiendo el archivo php para el index

---
+++ index.php
@@ -0,0 +1,16 @@
+// Mi primer php_check_syntax
+
+$a = 2
+$b = 3
+
+$suma = $a + $b
+$resta = $a - $b
+$multiplicacion = $a * $b
+$division = $a / $b
+echo 'La suma de las variables a y b es' . $suma;
+echo '<br>';
+echo 'La resta de las variables a y b es' . $resta;
+echo '<br>';
+echo 'La multiplicación de las variables a y b es' . $multiplicacion;
+echo '<br>';
+echo 'La división de las variables a y b es' . $division;
